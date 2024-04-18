a7fc36e7e181c9d68f0b8db39b003c6808461060
lsh <salee2@student.42seoul.kr>
2024-04-18 17:52:33 +0900

    feat: insertion sort

--- basic/insertionSort.cpp
+++ basic/insertionSort.cpp
@@ -9,29 +9,31 @@ void print(int* arr, int n){
     cout << '\n';
 }
 
-void insertionSort(int* arr, int n){
-}
-
 int main(){
 
     {
-        int arr[] = {1, 2, 4, 5, 3, 6};
+//        int arr[] = {1, 2, 4, 5, 3, 6};
+//        int arr[] = {1, 2, 3,4,5,6};
+        int arr[] = {6, 5, 4, 3, 2, 1};
+//        int arr[] = { 2, 1};
         int n = sizeof(arr) / sizeof(arr[0]);
         print(arr, n);
 
-        int i = 4;
-
-        int k = i - 1;
-        while (k >= 0 && arr[k] > arr[i]) {
-            --k;
-        }
-        ++k;
-
-        for (int j = i; j > k; --j) {
-            arr[j] = arr[j - 1];
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j;
+            for (j = i; j > 0 && arr[j - 1] > key; --j) {
+                arr[j] = arr[j - 1];
+                print(arr, n);
+            }
+            arr[j] = key;
             print(arr, n);
         }
-        print(arr, n);
-
     }
 }
+
+/*
+ * 1. insertion sort 구현 익숙하지 않아서 쉽지 않았다.
+ *
+ * 2. 이동하고 삽입할때 배열의 정확한 인덱스를 알아야함.
+ */
