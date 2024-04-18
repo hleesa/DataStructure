7db3cf8aaabae6ce9361b83ec4e3a59e18d134d6
lsh <salee2@student.42seoul.kr>
2024-04-18 16:58:15 +0900

    feat: shift

---
+++ basic/insertionSort.cpp
@@ -0,0 +1,30 @@
+ #include <iostream>
+
+using namespace std;
+
+void print(int* arr, int n){
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << '\n';
+}
+
+void insertionSort(int* arr, int n){
+}
+
+int main(){
+
+    {
+        int arr[] = {1, 2, 4, 5, 3, 6};
+        int n = sizeof(arr) / sizeof(arr[0]);
+        print(arr, n);
+
+        int i = 4;
+        for (int j = i; j > 0; --j) {
+            arr[j] = arr[j - 1];
+            print(arr, n);
+        }
+        print(arr, n);
+
+    }
+}
